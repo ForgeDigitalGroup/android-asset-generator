4cbdd3879e3d5b280630e8dbd8a00b22bfdc4125
DevLuuk <devluuk@gmail.com>
2021-06-16 19:09:05 +0200

    Added support for file reading for hexcolor

--- createBackgroundLayer.py
+++ createBackgroundLayer.py
@@ -1,13 +1,17 @@
-from PIL import Image
+from PIL import Image, ImageColor
 import sys
+
 # name of the file to save
-filename = "ic_launcher_background.png"
+importFilename = "background-color.txt"
+exportFilename = "ic_launcher_background.png"
 
 width = 500
 height = 500
-color1 = int(sys.argv[1])
-color2 = int(sys.argv[2])
-color3 = int(sys.argv[3])
 
-img = Image.new(mode="RGB", size=(width, height), color=(color1, color2, color3))
-img.save(filename)
+with open(importFilename) as file:
+    hexColor = file.readline()
+
+print('Color:', hexColor)
+
+img = Image.new(mode="RGB", size=(width, height), color=ImageColor.getrgb(hexColor))
+img.save(exportFilename)
